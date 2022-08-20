7624d3e879597884f3e888a2fd13ba9c08bc8c4e
ZafarKamal123 <sk4915497@gmail.com>
2022-08-20 18:32:16 +0500

    :wink: add domready expose

--- src/wordpress.ts
+++ src/wordpress.ts
@@ -5,5 +5,6 @@ export * as data from '@wordpress/data'
 export * as element from '@wordpress/element'
 export * as hooks from '@wordpress/hooks'
 export * as plugin from '@wordpress/plugins'
+export {default as domReady} from '@wordpress/dom-ready'
 
 export { default as serverSideRender } from '@wordpress/server-side-render'
