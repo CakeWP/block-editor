7c4f6cb6975b522765a78453b05080bbbabda3b2
ZafarKamal123 <sk4915497@gmail.com>
2022-08-24 13:55:39 +0500

    :+1: add usual wp deps

--- src/wordpress.ts
+++ src/wordpress.ts
@@ -6,6 +6,11 @@ export * as element from '@wordpress/element'
 export * as hooks from '@wordpress/hooks'
 export * as plugin from '@wordpress/plugins'
 export * as primitives from '@wordpress/primitives'
+export * as compose from '@wordpress/compose'
+export * as keycodes from '@wordpress/keycodes'
+export * as keyboardShortcut from '@wordpress/keyboard-shortcuts'
+export * as viewport from '@wordpress/viewport'
+
 export { default as domReady } from '@wordpress/dom-ready'
 
 export { default as serverSideRender } from '@wordpress/server-side-render'
